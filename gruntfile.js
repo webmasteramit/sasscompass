8f526cb3bff4b6d811254d9faf607fb5d1caefd0
Amit Sharma <Amit Sharma>
2016-06-03 17:49:08 +0530

    updated project with Images and files from 03-05 chapter

--- gruntfile.js
+++ gruntfile.js
@@ -3,8 +3,6 @@ module.exports= function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-compass');
-
-	
 	grunt.initConfig({
 		
 		uglify:{
